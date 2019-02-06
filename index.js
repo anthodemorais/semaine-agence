bfefea6c513e79153f1afb1ee07e09542655ff27
Anthony <anthonyd-m@live.com>
2019-02-06 16:06:54 +0100

    animation decouvrir app

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
-    
+    var canvas = document.querySelector("canvas");
+    var discover = document.querySelector("button");
+
+    var masque = document.querySelector("#masque");
+
+    discover.addEventListener("click", () =>
+    {
+        var wavesScript = document.querySelector('#wavesScript');
+        wavesScript.parentElement.removeChild(wavesScript);
+
+        masque.style.transition = "1.5s";
+        masque.style.top = "-630px";
+        masque.style.right = "-500px";
+        masque.style.width = "250%";
+        masque.style.height = "200%";
+
+        document.querySelector("button").style.display = "none";
+
+        setTimeout(() =>
+        {
+            masque.style.transition = "1s";
+            masque.style.backgroundColor = "white";
+            document.querySelector(".center").style.display = "none";
+            document.querySelector(".deco1").style.display = "none";
+            document.querySelector(".deco1_2").style.display = "none";
+            canvas.style.display = "none";
+        }, 1500);
+
+        setTimeout(() =>
+        {
+            document.querySelector("#masque").style.display = "none";
+            document.querySelector(".menu").style.zIndex = "4";
+            document.querySelector(".appPresentation").style.display = "flex";
+            document.querySelector(".scroll").style.display = "flex";
+            document.querySelector(".deco2").style.display = "block";
+        }, 2500);
+    });
 });
