2c5a53b3768513f1ab4eb83204726863a92f62a2
Anthony <anthonyd-m@live.com>
2019-02-06 21:22:45 +0100

    animations presentation app

--- discover.js
+++ discover.js
@@ -1,14 +1,32 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
+    var counter = 0;
     document.querySelector(".appPresentation").addEventListener("scroll", scrollEvent);
     document.querySelector(".scrollImage").addEventListener("click", scrollEvent);
+    var texts = document.querySelectorAll(".presText");
 
     function scrollEvent(event)
     {
-        document.querySelector(".firstPres").style.top = "-200px";
-        document.querySelector(".secondPres").style.top = "-200px";
-        document.querySelector(".thirdPres").style.top = "250px";
-        document.querySelector(".forthPres").style.top = "70px";
+        if ((counter + 4) <= (texts.length - 1))
+        {
+            if (counter % 2 == 0)
+            {
+                texts[counter].style.top = "-200px";
+                texts[counter+1].style.top = "-200px";
+                texts[counter+2].style.top = "250px";
+                texts[counter+3].style.top = "70px";
+            }
+            else
+            {
+                texts[counter+1].style.top = "-200px";
+                texts[counter+2].style.top = "-200px";
+                texts[counter+3].style.top = "70px";
+                texts[counter+4].style.top = "250px";
+            }
+
+            counter++;
+            event.preventDefault();
+        }
     }
 });
 
